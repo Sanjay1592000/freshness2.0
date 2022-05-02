dda0fd85d52e1601cc8b9567b72969fe1d954efe
Solai Raj <msraj085@gmail.com>
2022-05-02 13:23:35 +0530

    added post

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ def get_sensor_data():
         mq2_val) + "</p> <br> <p> MQ135 Value = " + str(mq15_val) + "</p>"
 
 
-@app.route('/detect', methods=['POST', 'GET'])
+@app.route('/detect', methods=['GET','POST'])
 def detect_image():
     fruit = request.form['fruit']
     capture()
